03b20228d7b8cc85a8820fba3c7ee14fa03acfee
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-04-20 18:45:22 +0800

    chore: add tests for config

--- config/viper/module.go
+++ config/viper/module.go
@@ -2,88 +2,128 @@ package viper_config
 
 import (
 	"log"
+	"os"
 	"strings"
 
 	"github.com/spf13/viper"
 )
 
+const (
+	defaultConfigFileType = "yaml"
+	defaultPrefix         = ""
+	defaultPrintDebugLogs = false
+)
+
+var defaultConfigFilePaths = []string{"./", "./configs"}
+
 type Config struct {
 }
 
-func NewConfig(prefix string, printDebugLogs bool) *Config {
-
-	//SETUP -----------------------------
+func SetUpConfig(prefix string, configFileType string, printDebugLogs bool) *Config {
+	//environmental variables
 	if prefix != "" {
 		viper.SetEnvPrefix(prefix)
-	} else {
-		log.Printf("No prefix set for config")
 	}
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	//CONFIG FILES -------------------------------------
-	viper.AddConfigPath("./")
-	viper.AddConfigPath("./configs")
+	//config file type
+	validatedConfigFileType := validateConfigFileType(configFileType)
+	viper.SetConfigType(validatedConfigFileType)
 
-	readConfigWithOptionalOverride("config", "config.override")
+	//config file paths
+	for _, path := range defaultConfigFilePaths {
+		viper.AddConfigPath(path)
+	}
+
+	readConfigWithOptionalOverride("config", "config.override", validatedConfigFileType)
 
 	config := &Config{}
 
 	if printDebugLogs {
-		config.PrintDebugLogs(prefix)
+		config.PrintDebugLogs(prefix, validatedConfigFileType)
 	}
 
 	return config
 }
 
-func (c *Config) SetConfigs(configs map[string]interface{}) {
-
-	for k, v := range configs {
-		if !viper.IsSet(k) {
-			viper.Set(k, v)
-		}
+func readConfigWithOptionalOverride(baseConfig string, overrideConfig string, configFileType string) {
+	if configFileType == "" {
+		configFileType = "yaml"
 	}
-}
 
-func (c *Config) PrintDebugLogs(prefix string) {
-	log.Println("----- Config Setup -----")
-	log.Printf("|| Prefix   %s", prefix)
-	log.Printf("|| replacer %s", ". -> _")
-	log.Printf("|| autoEnv  %s", "true")
-	log.Printf("|| name     %s", "config")
-	log.Printf("|| path     %s", "./ OR ./configs")
-	log.Println("------------------------")
-}
+	//check for presence of config files
+	_, err := os.Stat(baseConfig + "." + configFileType)
+	if err != nil {
+		log.Printf("%s.%s not found. No config file loaded.", baseConfig, configFileType)
+		return
+	}
 
-// readConfigs reads and applies the configuration files using Viper.
-// It takes two parameters: baseConfig (the name of the primary config file) and overrideConfig (the name of the override config file).
-// If the primary config file exists, it is read and applied. If the override config file exists, it is merged with the primary config file.
-func readConfigWithOptionalOverride(baseConfig string, overrideConfig string) {
 	// read the primary config file
 	viper.SetConfigName(baseConfig)
-	err := viper.ReadInConfig()
+	err = viper.ReadInConfig()
 	if err != nil {
-		_, ok := err.(viper.ConfigFileNotFoundError)
-		if !ok {
-			log.Printf("Error reading config file, %s", err)
-		} else {
-			log.Println("No base config")
-		}
+		log.Printf("Error reading base config file, %s. No config file loaded.", err)
 	} else {
 		log.Printf("Base config applied: %s\n", viper.ConfigFileUsed())
+	}
+
+	// check for presence of override config file
+	_, err = os.Stat(overrideConfig + "." + configFileType)
+	if err != nil {
+		log.Printf("%s.%s not found. No override configuration loaded.", overrideConfig, configFileType)
+		return
 	}
 
 	// merge if override config file if it exists
 	viper.SetConfigName(overrideConfig)
 	err = viper.MergeInConfig()
 	if err != nil {
-		_, ok := err.(viper.ConfigFileNotFoundError)
-		if !ok {
-			log.Printf("Error reading override config file, %s", err)
-		} else {
-			log.Println("No override config")
-		}
+		log.Printf("Error reading override config file, %s. No override configuration loaded.", err)
 	} else {
 		log.Printf("Override config applied: %s\n", viper.ConfigFileUsed())
+	}
+}
+
+func validateConfigFileType(configFileType string) string {
+	if configFileType == "" {
+		return defaultConfigFileType
+	}
+
+	//JSON, TOML, YAML, HCL, envfile
+	if configFileType != "json" &&
+		configFileType != "toml" &&
+		configFileType != "yaml" &&
+		configFileType != "hcl" &&
+		configFileType != "envfile" {
+		log.Printf("Invalid config file type: %s. Defaulting to yaml", configFileType)
+		return defaultConfigFileType
+	}
+
+	return configFileType
+}
+
+// ---------------------------------------------------------
+
+func (c *Config) PrintDebugLogs(prefix string, configFileType string) {
+	log.Println("----- Config Setup -----")
+	log.Printf("|| Prefix   %s", prefix)
+	log.Printf("|| replacer %s", ". -> _")
+	log.Printf("|| autoEnv  %s", "true")
+	log.Printf("|| name     %s", "config OR config.override")
+	log.Printf("|| paths    %s", defaultConfigFilePaths)
+	log.Printf("|| fileType %s", configFileType)
+	log.Println("------------------------")
+}
+
+// ---------------------------------------------------------
+
+// Fallback configurations will be applied if there is no config file or environment variables
+func (c *Config) SetFallbackConfigs(configs map[string]interface{}) {
+
+	for k, v := range configs {
+		if !viper.IsSet(k) {
+			viper.Set(k, v)
+		}
 	}
 }
